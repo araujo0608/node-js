0b9d0d78c2f9e029d8594508bc16f06ba388009b
araujo0608 <68195900+araujo0608@users.noreply.github.com>
2022-02-27 11:21:32 -0300

    update

--- #004 - HTTP PROTOCOL/app.js
+++ #004 - HTTP PROTOCOL/app.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 http.createServer(function(req, res){
-    res.end("Hello!");
+    res.end("Hello World!");
 }).listen(8081);
 
-console.log("server running on 8081... \nvisit localhost:8081");
+console.log("server running on localhost:8081...");
